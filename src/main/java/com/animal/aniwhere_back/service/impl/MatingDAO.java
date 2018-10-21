e5977de78c4d2e9151ded65e77be26b8f8a8cdcd
kiru <kiru@CocktailPC>
2018-10-22 00:58:25 +0900

    sub query 추가
    group buy 제외

--- src/main/java/com/animal/aniwhere_back/service/impl/MatingDAO.java
+++ src/main/java/com/animal/aniwhere_back/service/impl/MatingDAO.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import com.animal.aniwhere_back.service.AllBoardService;
 import com.animal.aniwhere_back.service.MatingDTO;
+import com.animal.aniwhere_back.service.member.AnimalDTO;
 
 @Repository
 public class MatingDAO implements AllBoardService {
@@ -47,5 +48,9 @@ public class MatingDAO implements AllBoardService {
 	public int delete(Map map) {
 		return template.delete("matingDelete", map);
 	}////////// delete
+	
+	public List<AnimalDTO> selectMyMating(Map map){
+		return template.selectList("selectMyMating", map);
+	}////////// selectMyMating
 
 }//////////////////// MatingBoardDAO
