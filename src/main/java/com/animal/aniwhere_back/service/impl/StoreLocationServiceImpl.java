b94b1804f08beab410ec5e49709a3838a82ad6e5
kirucas <kolp00103@gmail.com>
2018-10-23 01:24:39 +0900

    일부 DTO/query 수정 | DTO/DAO/Service/query문 생성
    수정
     - group_buy 관련
     - member query문 일부 수정
    
    생성
     - store_location / group_buy list / lost_animal(DAO/Service/query문 제외)

---
+++ src/main/java/com/animal/aniwhere_back/service/impl/StoreLocationServiceImpl.java
@@ -0,0 +1,50 @@
+package com.animal.aniwhere_back.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.animal.aniwhere_back.service.AllBoardService;
+import com.animal.aniwhere_back.service.StoreLocationDTO;
+
+@Service("StoreLocService")
+public class StoreLocationServiceImpl implements AllBoardService {
+
+	@Resource(name = "storeLocationDAO")
+	private StoreLocationDAO dao;
+
+	@Override
+	public List<StoreLocationDTO> selectList(Map map) {
+		return dao.selectList(map);
+	}////////// selectList
+
+	@Override
+	public int getTotalRecord(Map map) {
+		return dao.getTotalRecord(map);
+	}////////// getTotalRecord
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public StoreLocationDTO selectOne(Map map) {
+		return dao.selectOne(map);
+	}////////// selectOne
+
+	@Override
+	public int insert(Map map) {
+		return dao.insert(map);
+	}////////// insert
+
+	@Override
+	public int update(Map map) {
+		return dao.update(map);
+	}////////// update
+
+	@Override
+	public int delete(Map map) {
+		return dao.delete(map);
+	}////////// delete
+
+}//////////////////// StoreLocationServiceImpl class
