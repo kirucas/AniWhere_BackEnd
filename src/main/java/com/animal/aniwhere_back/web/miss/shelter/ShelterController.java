df3719cc5ca3b3b29917b6741d1f73f60ec0a901
Truck <bumsu0813@gmail.com>
2018-10-30 17:35:35 +0900

    변한거없음

--- src/main/java/com/animal/aniwhere_back/web/miss/shelter/ShelterController.java
+++ src/main/java/com/animal/aniwhere_back/web/miss/shelter/ShelterController.java
@@ -1,10 +1,17 @@
 package com.animal.aniwhere_back.web.miss.shelter;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.animal.aniwhere_back.service.impl.miss.LostAnimalServiceImpl;
+
 @Controller
 public class ShelterController {
+	
+	@Resource(name="lostAniService")
+	private LostAnimalServiceImpl shelterService;
 	
 	@RequestMapping("/miss/shelter.aw")
 	public String shelter() throws Exception {
